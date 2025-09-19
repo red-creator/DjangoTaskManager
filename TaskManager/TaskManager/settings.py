a70a2aec308c75c24f9acec888169a6ee52c8ae3
red-creator <grivankov@gmail.com>
2025-09-19 22:44:03 +0300

    Add requirements.txt and update settings

--- TaskManager/TaskManager/settings.py
+++ TaskManager/TaskManager/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v#5ju4%llsply1(0_k0jby28hhad12!*q-atl7m64lx#&@g6$1'
+SECRET_KEY = os.getenv('SECRET_KEY', 'optional-default-for-dev')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = []
 
